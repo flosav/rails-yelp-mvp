ead604a4147fc5f2ccc9fe234b764d21e08078e4
Maero Floriane <floriane.maero@gmail.com>
2021-03-04 12:12:23 +0100

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Restaurant.destroy_all
-bristol = Restaurant.create(name: "Epicure", address: "32 blabla", phone_number: "0432A", category: "french")
-bristol1 = Restaurant.create(name: "Epicure", address: "32 blabla", phone_number: "0432A", category: "french")
-bristol2 = Restaurant.create(name: "Epicure", address: "32 blabla", phone_number: "0432A", category: "french")
-bristol3 = Restaurant.create(name: "Epicure", address: "32 blabla", phone_number: "0432A", category: "french")
